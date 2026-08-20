2a3016491c30e0799655d15bf47509ea52760756
dipak1920basnet <dipak1920basnet@gmail.com>
2026-08-20 15:34:20 +0545

    add a blue print to convert unit to meter and meter to other unit

---
+++ meter_convert.py
@@ -0,0 +1,40 @@
+def millimeter_to_meter(millimeter:float|int)->float|int:
+    pass
+
+def centimeter_to_meter(centimeter:float|int)->float|int:
+    pass
+
+def kilometer_to_meter(kilometer:float|int)->float|int:
+    pass
+
+def inch_to_meter(inch:float|int)->float|int:
+    pass
+
+def foot_to_meter(foot:float|int)->float|int:
+    pass
+
+def yard_to_meter(yard:float|int)->float|int:
+    pass
+
+def mile_to_meter(mile:float|int)->float|int:
+    pass
+
+
+def meter_to_millimeter(meter:float|int)->float|int:
+    pass
+
+
+def meter_to_centimeter(meter:float|int)->float|int:
+    pass
+
+def meter_to_kilometer(meter:float|int)->float|int:
+    pass
+
+def meter_to_foot(meter:float|int)->float|int:
+    pass
+
+def meter_to_yard(meter:float|int)->float|int:
+    pass
+
+def meter_to_mile(meter:float|int)->float|int:
+    pass
